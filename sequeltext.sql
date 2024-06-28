43fc284e1f7808eb77abf70b10de0b7c50fe0a39
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-06-28 04:47:29 -0500

    Update sequeltext.sql

--- sequeltext.sql
+++ sequeltext.sql
@@ -1,6 +1,11 @@
 myHost, 127.0.0.1
 myInstance, host
 myPort, 1955
+  column in data "shoe size",
+  column in data "red, blue, or green",
+  column in data "name & address",
+  add column in data NULL,
 column in data "description of tattoo",
 column in data "name",
 column in data "number,
+
